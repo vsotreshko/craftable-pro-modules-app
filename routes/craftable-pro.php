04e0014ecac98de9f10be180ed6b9d155c36f290
vsotreshko <vsotreshko@gmail.com>
2024-05-09 15:04:02 +0200

    :sparkles: feat: Example module setup

--- routes/craftable-pro.php
+++ routes/craftable-pro.php
@@ -1,5 +1,16 @@
 <?php
 
+use Brackets\LaravelModuleComposerPackage\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::craftablePro('admin');
+
+Route::middleware('craftable-pro-middlewares')->prefix('admin')->name('craftable-pro.')->group(function () {
+    Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
+    Route::get('orders/create', [OrderController::class, 'create'])->name('orders.create');
+    Route::post('orders', [OrderController::class, 'store'])->name('orders.store');
+    Route::get('orders/edit/{order}', [OrderController::class, 'edit'])->name('orders.edit');
+    Route::match(['put', 'patch'], 'orders/{order}', [OrderController::class, 'update'])->name('orders.update');
+    Route::delete('orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
+    Route::post('orders/bulk-destroy', [OrderController::class, 'bulkDestroy'])->name('orders.bulk-destroy');
+});
